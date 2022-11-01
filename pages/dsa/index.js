176d04d8aa68da77f13a5a8cd3da3f7af6b02514
yatharth <yatharthverma070@gmail.com>
2022-11-01 16:34:57 +0530

    Add sanity

--- pages/dsa/index.js
+++ pages/dsa/index.js
@@ -1,7 +1,28 @@
-export default function DSA() {
+import { createClient } from "next-sanity";
+
+const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: "2022-03-25",
+  useCdn: false,
+});
+
+export default function DSA({ dsaConcepts }) {
   return (
     <div className="p-10 ">
-      <h1>Coming soon ...</h1>
+      {dsaConcepts.map((item) => (
+        <details>{item?.name}</details>
+      ))}
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const dsaConcepts = await client.fetch(`*[_type == "dsaConcepts"]`);
+
+  return {
+    props: {
+      dsaConcepts,
+    },
+  };
 }
