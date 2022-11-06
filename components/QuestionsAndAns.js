a74392a5db9d2642c807dc6daa892a8a2b1d6d98
yatharth <yatharthverma070@gmail.com>
2022-11-06 15:36:02 +0530

    Integrated markdown and syntax highlighting

--- components/QuestionsAndAns.js
+++ components/QuestionsAndAns.js
@@ -1,14 +1,36 @@
 import React from "react";
-import { PortableText } from "@portabletext/react";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import dark from "react-syntax-highlighter/dist/cjs/styles/prism/coldark-cold";
 
 function QuestionsAndAns({ questions }) {
   console.log(questions);
   return questions?.map((que, index) => (
     <div key={index} className="mb-6 text-sm">
       <h4 className="font-bold mt-2 mb-2 text-lg">
-        Q{index + 1}. {que?.question}
+        {index + 1}. {que?.question}
       </h4>
-      <PortableText value={que?.answer} />
+      <ReactMarkdown
+        children={que?.answer}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, "")}
+                style={dark}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      />
     </div>
   ));
 }
